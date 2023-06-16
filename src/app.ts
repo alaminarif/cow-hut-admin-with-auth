80a15f394054996f14a2ab1c3df7d36f96185886
al-amin <arifurr231@gmail.com>
2023-06-16 08:34:15 +0600

    project setup

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, urlencoded } from 'express';
+import cors from 'cors';
+import globalErrodHandler from './app/middleware/GlobalErrorHandler';
+import router from './app/router';
+const app: Application = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
+app.use('/api/v1', router);
+
+// app.get('/', async (req: Request, res: Response, next: NextFunction) => {
+//   Promise.reject(new Error('Unhandle Promise Rejection'));
+// });
+
+app.use(globalErrodHandler);
+export default app;
